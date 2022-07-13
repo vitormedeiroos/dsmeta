16d70eb8805ee67b7b39249cb11ce7a476f4e6f6
Vitor Macedo <vitoredaurdomacedo0@gmail.com>
2022-07-13 14:01:45 -0300

    Project Clean

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './index.css'// esses import's são pra usar recursos de putros apps 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
