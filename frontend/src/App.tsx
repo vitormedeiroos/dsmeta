e92ce9c73e3b5376bcdbaf47f6dd7e11f1895907
Vitor Macedo <vitoredaurdomacedo0@gmail.com>
2022-07-14 22:01:34 -0300

    Primeiro Componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+import NotificationButton from "./componentes/NotificationButton"
+//  se colocar mais de um na função vai dar errado,porque o compopnente react não pode importar mais de uma tag o h1 e o notification
 function App() {
-  return(
-    <h1>Olá Mundo!</h1>
-  )//aqui vai delarar a função e dentro dela vai retornar um conteúdo
-  }
+  return (
+    <>
+      <h1>Olá Mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+    //vai chamar a função que eu criei no index.tsx, notification button
+    // esse <> </> (fragmnent) serve para validar o componente e funcionar os componetes que vc colocar 
+  )//aqui vai declarar a função e dentro dela vai retornar um conteúdo 
+  // nesse caso vai dar vermelho porque o compopnente react não pode importar mais de uma tag o h1 e o notification button
+}
 export default App // no React o componente visual que vc vai ver no site vai ser uma função do javascript
---
+++ frontend/src/componentes/NotificationButton/index.tsx
@@ -0,0 +1,12 @@
+import icon from '../../assets/img/notification-icon.svg'
+import './styles.css'
+    //mesma pasta é . / pasta anterior é ..
+    function NotificationButton() {
+        // aqui eu criei a função notificationButton e onde eu chamar ela vai aparecer o que eu coloquei aqui
+        return (
+            <div className="dsmeta-red-btn">
+                <img src={icon} alt="Notificar" />
+            </div>
+        )
+    }
+export default NotificationButton // copiado do app.tsx
