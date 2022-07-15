7c98b9402997c02d87d82620e8b38a97318d2b2c
Vitor Macedo <vitoredaurdomacedo0@gmail.com>
2022-07-15 01:15:49 -0300

    Outros Componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
-import NotificationButton from "./componentes/NotificationButton"
+import Header from "./componentes/Header"
+import SalesCard from "./componentes/SalesCard"
 //  se colocar mais de um na função vai dar errado,porque o compopnente react não pode importar mais de uma tag o h1 e o notification
 function App() {
   return (
     <>
-      <h1>Olá Mundo!</h1>
-      <NotificationButton />
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
     //vai chamar a função que eu criei no index.tsx, notification button
-    // esse <> </> (fragmnent) serve para validar o componente e funcionar os componetes que vc colocar 
-  )//aqui vai declarar a função e dentro dela vai retornar um conteúdo 
-  // nesse caso vai dar vermelho porque o compopnente react não pode importar mais de uma tag o h1 e o notification button
+    // esse <> </> (fragmnent) serve para validar o componente e funcionar os componetes que vc colocar
+  ) //aqui vai declarar a função e dentro dela vai retornar um conteúdo
+  // não pode retornar masi de um componente dentro de uma function
 }
-export default App // no React o componente visual que vc vai ver no site vai ser uma função do javascript
+export default App // no React o componente visual que vc vai ver no site é uma função do js
