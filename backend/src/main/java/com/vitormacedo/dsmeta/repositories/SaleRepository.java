56000ba56fda8df457456d56c32893e945a7820b
Vitor Macedo <vitoredaurdomacedo0@gmail.com>
2022-07-24 15:32:18 -0300

    API test

---
+++ backend/src/main/java/com/vitormacedo/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.vitormacedo.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vitormacedo.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
---
+++ backend/src/main/java/com/vitormacedo/dsmeta/services/SaleService.java
@@ -0,0 +1,20 @@
+package com.vitormacedo.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vitormacedo.dsmeta.entities.Sale;
+import com.vitormacedo.dsmeta.repositories.SaleRepository;
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+
+	public List<Sale> findSales() {
+		return repository.findAll();
+		
+	}
+}
